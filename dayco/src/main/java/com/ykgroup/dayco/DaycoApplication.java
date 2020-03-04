d19d15662ca971fe3e718ed716baefcc57a3b66b
yanghun0070 <yanghun0070@gmail.com>
2020-03-04 23:03:46 +0900

    init project

---
+++ dayco/src/main/java/com/ykgroup/dayco/DaycoApplication.java
@@ -0,0 +1,13 @@
+package com.ykgroup.dayco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaycoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaycoApplication.class, args);
+	}
+
+}
