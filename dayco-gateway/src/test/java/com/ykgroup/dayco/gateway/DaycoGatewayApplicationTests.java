7cdb9fa7f59beb305acec779ad26d0aef9046715
yanghun0070 <yanghun0070@gmail.com>
2020-03-22 00:34:17 +0900

    Fix actuator url in ribbonPing and Seperate Gateway And Registry

---
+++ dayco-gateway/src/test/java/com/ykgroup/dayco/gateway/DaycoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ykgroup.dayco.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaycoGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
