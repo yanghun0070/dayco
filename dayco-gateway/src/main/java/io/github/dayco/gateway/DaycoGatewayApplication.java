3adb25658c23af4e8121cbb4ac5d8647ca218b47
yanghun0070 <yanghun0070@gmail.com>
2020-09-07 22:56:55 +0900

    Use Spring Cloud Gateway instead of Zuul

--- dayco-gateway/src/main/java/io/github/dayco/gateway/DaycoGatewayApplication.java
+++ dayco-gateway/src/main/java/io/github/dayco/gateway/DaycoGatewayApplication.java
@@ -2,15 +2,7 @@ package io.github.dayco.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-import io.github.dayco.gateway.config.RibbonConfiguration;
-
-@EnableZuulProxy
-@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
-@EnableEurekaClient
 @SpringBootApplication
 public class DaycoGatewayApplication {
 
