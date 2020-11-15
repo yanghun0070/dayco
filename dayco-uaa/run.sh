233ec38545bbe8e4da61d917d7279d77c40a15fb
yanghun0070 <yanghun0070@gmail.com>
2020-11-15 17:49:46 +0900

    Add Dockerfile to UAA service

---
+++ dayco-uaa/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#이미지를 빌드하는 명령어는 생성할 이미지 명
+docker build --tag dayco:1.0 .
+
+# Docker Image 목록
+docker images
+
+# 도커이미지 잘 동작하는 지 컨테이너 확인
+docker run -p 9999:9999 -it dayco:1.0 /bin/bash
+
